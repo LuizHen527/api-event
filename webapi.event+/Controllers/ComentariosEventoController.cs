96c55898ef0a59d6bdcccafc6ef8163b1f1cce50
Luiz <luiz.henrique31415@gmail.com>
2023-12-08 11:02:48 -0300

    s

---
+++ webapi.event+/Controllers/ComentariosEventoController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventoController : ControllerBase
+    {
+        ComentariosEventoRepository comentarios = new ComentariosEventoRepository();
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(comentarios.Listar());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("BuscarPorIdUsuario")]
+
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(comentarios.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Post(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                comentarios.Cadastrar(novoComentario);
+
+                return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                comentarios.Deletar(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
